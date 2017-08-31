1d86f2e176e9e5cb33a38f272610e3d8d328c603
陆恺 <lukai@lukaideMacBook-Pro.local>
2017-08-31 17:58:13 +0800

    scanner插件

--- saikillua98/scanner/src/Scan.php
+++ saikillua98/scanner/src/Scan.php
@@ -1,12 +1,10 @@
 <?php
+namespace Saikillua\Scanner;
 
-/**
- *
- * User: lk(lk@joinbe.com)
- * Date: 2017/8/31
- * Time: 下午5:53
- */
 class Scan
 {
-
+    public function run()
+    {
+        echo "run success";
+    }
 }
